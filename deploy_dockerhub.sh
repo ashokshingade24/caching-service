69339c2a82b59fa46bea9e43a39c170f887cbfa0
Admin <email@gmail.com>
2019-04-08 15:11:25 +0530

    Add .drone.yml

---
+++ deploy_dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker login --username $DOCKER_USER --password $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker tag $TRAVIS_REPO_SLUG ashokshingade24/cicd:$TAG
+docker push ashokshingade24/cicd:$TAG
