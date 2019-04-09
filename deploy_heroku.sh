ee50b685b9e802047d37cdeacb0d3e49bb97fc15
ashokshingade24 <ashokshingade24@gmail.com>
2019-04-09 08:36:23 +0530

    Update deploy_heroku.sh

--- deploy_heroku.sh
+++ deploy_heroku.sh
@@ -1,4 +1,4 @@
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login --username _ --password=634f900f-2179-4853-bd59-74e3e6a772b0 registry.heroku.com
+docker login --username _ --password= registry.heroku.com
 heroku container:push web --app pure-fjord-93329
 heroku container:release web --app pure-fjord-93329
